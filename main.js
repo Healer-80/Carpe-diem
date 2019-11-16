f1e911aac8e9b24a49083f10e5ea92def06ea90c
Healer-80 <larysa.rudzinska@gmail.com>
2019-11-16 20:50:35 +0000

    instal Firebase app

---
+++ main.js
@@ -0,0 +1,29 @@
+(function () {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB79euw8NtFWlD4DXrcH5sHslLUdN7S4Vs",
+    authDomain: "carpe-diem-5b9d6.firebaseapp.com",
+    databaseURL: "https://carpe-diem-5b9d6.firebaseio.com",
+    projectId: "carpe-diem-5b9d6",
+    storageBucket: "carpe-diem-5b9d6.appspot.com",
+    messagingSenderId: "452338796470",
+    appId: "1:452338796470:web:fe147c19c515aca424a317"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }());
